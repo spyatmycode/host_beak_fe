58ae713efbc5cf5e05bd65bf1871cb392805d87e
spyatmycode <akejunifemi11@gmail.com>
2024-03-05 23:37:24 +0100

    404

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -30,6 +30,12 @@ const Router = () => {
       path: authRoute,
       element: <Auth />,
     },
+    {
+      path:"*",
+      element: <div className="w-full h-screen flex justify-center items-center text-red-500">
+        Page Not Found (404 error).
+      </div>,
+    },
   ]);
   return (
     <AppProvider>
